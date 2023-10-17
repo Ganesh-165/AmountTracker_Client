15b3540f11566877fe01d3fca1bb26202382caaf
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-10-17 21:34:54 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://amounttracker-server.onrender.com',
     headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+    withCredentials: true,
+    credentials: 'include'
 });
